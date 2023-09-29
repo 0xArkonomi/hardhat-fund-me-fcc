e3baeefbe155f0cd11860c3296dfd44da6a455bc
Mohammad Mahdi Keshavarz <arkonomii@gmail.com>
2023-09-29 16:00:27 +0330

    Added NatSpec comments and optimized codes

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -7,33 +7,42 @@ import "./PriceConverter.sol";
 error FundMe__NotOwner();
 
 /**@title A sample Funding Contract
- * @author Patrick Collins
- * @notice This contract is for creating a sample funding contract
+ * @author Patrick Collins - (Mohammad Mahdi Keshavarz Edition)
+ * @notice Decentralized Funding Smart Contract Sample 
  * @dev This implements price feeds as our library
  */
 contract FundMe {
     // Type Declarations
     using PriceConverter for uint256;
-
-    // State variables
+    
+    /// @notice Minimum required funding in USD-equivalent Ether.
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
-    address private immutable i_owner;
-    address[] private s_funders;
-    mapping(address => uint256) private s_addressToAmountFunded;
-    AggregatorV3Interface private s_priceFeed;
+
+    /// @dev The address of the contract owner who can withdraw funds.
+    address private immutable owenr;
+
+    /// @dev An array of addresses representing funders who have contributed to the contract.
+    address[] private funders; 
+
+    /// @dev A mapping that associates each funder's address with the amount they've funded.
+    mapping(address => uint256) private addressToAmountFunded;
+
+    /// @dev An instance of the Chainlink Price Feed interface used for price conversion.
+    AggregatorV3Interface private priceFeed;
+
 
     // Events (we have none!)
 
     // Modifiers
     modifier onlyOwner() {
-        // require(msg.sender == i_owner);
-        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        // require(msg.sender == owenr);
+        if (msg.sender != owenr) revert FundMe__NotOwner();
         _;
     }
 
-    constructor(address priceFeed) {
-        s_priceFeed = AggregatorV3Interface(priceFeed);
-        i_owner = msg.sender;
+    constructor(address _priceFeed) {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        owenr = msg.sender;
     }
 
 
@@ -47,45 +56,46 @@ contract FundMe {
     /// @notice Funds our contract based on the ETH/USD price
     function fund() public payable {
         require(
-            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
             "You need to spend more ETH!"
         );
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        s_addressToAmountFunded[msg.sender] += msg.value;
-        s_funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
 
     function withdraw() public onlyOwner {
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < s_funders.length;
-            funderIndex++
-        ) {
-            address funder = s_funders[funderIndex];
-            s_addressToAmountFunded[funder] = 0;
-        }
-        s_funders = new address[](0);
-        // Transfer vs call vs Send
-        // payable(msg.sender).transfer(address(this).balance);
-        (bool success, ) = i_owner.call{value: address(this).balance}("");
-        require(success);
-    }
-
-    function cheaperWithdraw() public onlyOwner {
-        address[] memory funders = s_funders;
-        // mappings can't be in memory, sorry!
         for (
             uint256 funderIndex = 0;
             funderIndex < funders.length;
             funderIndex++
         ) {
             address funder = funders[funderIndex];
-            s_addressToAmountFunded[funder] = 0;
+            addressToAmountFunded[funder] = 0;
         }
-        s_funders = new address[](0);
+        funders = new address[](0);
+        // Transfer vs call vs Send
         // payable(msg.sender).transfer(address(this).balance);
-        (bool success, ) = i_owner.call{value: address(this).balance}("");
-        require(success);
+        (bool success, ) = owenr.call{value: address(this).balance}("");
+        require(success, "Withdrawal failed.");
+    }
+
+    function cheaperWithdraw() public onlyOwner {
+        address[] memory tempFunders = funders;
+        // mappings can't be in memory, sorry!
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < tempFunders.length;
+            funderIndex++
+        ) {
+            address funder = tempFunders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // payable(msg.sender).transfer(address(this).balance);
+        (bool success, ) = owenr.call{value: address(this).balance}("");
+        require(success, "cheaperWithdraw failed.");
+
     }
 
     /** @notice Gets the amount that an address has funded
@@ -97,22 +107,22 @@ contract FundMe {
         view
         returns (uint256)
     {
-        return s_addressToAmountFunded[fundingAddress];
+        return addressToAmountFunded[fundingAddress];
     }
 
     function getVersion() public view returns (uint256) {
-        return s_priceFeed.version();
+        return priceFeed.version();
     }
 
     function getFunder(uint256 index) public view returns (address) {
-        return s_funders[index];
+        return funders[index];
     }
 
     function getOwner() public view returns (address) {
-        return i_owner;
+        return owenr;
     }
 
     function getPriceFeed() public view returns (AggregatorV3Interface) {
-        return s_priceFeed;
+        return priceFeed;
     }
 }
