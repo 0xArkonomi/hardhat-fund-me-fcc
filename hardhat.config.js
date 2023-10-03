50623bd92ac900623f74a023c2486103eed925d9
Mohammad Mahdi Keshavarz <arkonomii@gmail.com>
2023-10-03 23:45:22 +0330

    Update hardhat.config.js
    Add gas reporter and it's require. Add solidity-coverage require

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,8 @@ require("@nomiclabs/hardhat-waffle")
 require("dotenv").config()
 require("hardhat-deploy")
 require("@nomiclabs/hardhat-etherscan")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 const SEPOLIA_RPC_URL =
     process.env.SEPOLIA_RPC_URL ||
@@ -10,38 +12,46 @@ const PRIVATE_KEY =
     process.env.PRIVATE_KEY ||
     "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 
 module.exports = {
   defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-        chainId: 31337,
-        // gasPrice: 130000000000,
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
     },
-    sepolia: {
-        url: SEPOLIA_RPC_URL,
-        accounts: [PRIVATE_KEY],
-        chainId: 11155111,
-        blockConfirmations: 6,
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.19",
+            },
+            {
+                version: "0.6.6",
+            },  
+        ],
     },
-},
-solidity: {
-    compilers: [
-    {
-        version: "0.8.19",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
     },
-    {
-        version: "0.6.6",
-    },  
-  ],
-},
-namedAccounts: {
-    deployer: {
-        default: 0,
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+        // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
     },
-},
-etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-    // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
-},
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        // outputFile: "gas-report.txt",
+        // noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
 };
